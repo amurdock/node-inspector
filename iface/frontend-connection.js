54c43416dea69b60423e72e72114d925dac93ef1
Alasdair Murdock <alasdair.murdock@mudaless.com>
2015-10-25 15:35:24 +0000

    add ability to handle events and include 'getResourceContent' on api

--- iface/frontend-connection.js
+++ iface/frontend-connection.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const util       = require('util')
-	, EventEmitter = require('events')
+	, EventEmitter = require('eventemitter2').EventEmitter2
 	, callback     = require('./callback');
 
 function FrontendConnection() {
-	EventEmitter.call(this);
+	EventEmitter.call(this, {
+		wildcard: true
+	});
 	this._callback = callback();
 }
 
@@ -33,6 +35,25 @@ FrontendConnection.prototype.getResourceTree = function getResourceTree(cb) {
 	this.emit('message', payload);
 };
 
+/**
+ *
+ * @param url
+ * @param cb
+ */
+FrontendConnection.prototype.getResourceContent = function getResourceContent(url, cb) {
+	var id = this._callback.wrap(cb)
+		, payload = JSON.stringify({
+			id: id,
+			method: 'Page.getResourceContent',
+			params: {
+				frameId: 'nodeinspector-toplevel-frame',
+				url: url
+			}
+		});
+
+	this.emit('message', payload);
+};
+
 /// More methods to come .... also need to sort out what we do with events!!!
 
 /**
@@ -44,7 +65,14 @@ FrontendConnection.prototype.send = function send(payload) {
 		, id      = message.id
 		, args    = [ id ];
 
-	if (id) {
+	if (!id) {
+		var dotCase = require('dot-case')
+			, method  = dotCase(message.method);
+
+		//console.log(method);
+
+		this.emit(method, message);
+	} else {
 		if (message.result) {
 			args.push(message.result);
 		}
--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const Session            = require('./lib/session')
 var connection = new FrontendConnection()
 	, session    = new Session({}, 5858, connection);
 
+function debuggerEvent(payload) {
+	// console.log('%s fired', this.event);
+}
+
+// Debugger events
+connection.on('debugger.*',   debuggerEvent);
+connection.on('debugger.*.*', debuggerEvent);
+
 connection.startup(function() {
 	connection.getResourceTree(function(resourceTree) {
 		var frameTree = resourceTree.frameTree
@@ -19,6 +27,16 @@ connection.startup(function() {
 		console.log('First 10 of %s resource(s) ....', resources.length);
 		resources.slice(0, 10).forEach(function(resource) {
 			console.log('\t%s ~ %s', resource.type, resource.url);
+		});
+
+		//
+		var sampleFilename = 'node-inspector/sample.js';
+		var sampleUrl      = resources.filter(function(resource) {
+			return resource.url.indexOf(sampleFilename, resource.url.length - sampleFilename.length) !== -1
+		})[0].url;
+
+		connection.getResourceContent(sampleUrl, function(source) {
+			console.log('source for %s ~\n\n%s', sampleUrl, source.content);
 		});
 	});
 });
